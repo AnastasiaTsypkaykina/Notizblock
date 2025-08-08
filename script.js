b46a00e76caf6895bde0112c5b60924ed7c69468
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-08-08 13:02:45 +0200

    edited local storage

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-let notes = ["Banana", "Rasen mähen"];
+let notes = ["Banana", "Orange", "Rasen mähen", "Abendessen vorbereiten"];
+let notesTitle = ["Einkaufsliste", "", "Aufgaben", ""];
 
 function renderNotes() {
-    let contentRef = document.getElementById("content");
-    contentRef.innerHTML="";
-    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-        const note = notes[indexNote];   
-        contentRef.innerHTML += getNoteTemplate(indexNote);     
-    }    
+  let contentRef = document.getElementById("content");
+  contentRef.innerHTML = "";
+  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+    const note = notes[indexNote];
+    contentRef.innerHTML += getNoteTemplate(indexNote);    
+  }
+  getFromLocalStorage();
 }
 
 function getNoteTemplate(indexNote) {
-    return `<p>- ${notes[indexNote]} <button onclick="deleteNote(${indexNote})"> x </button> </p`
+  return `<h2>${notesTitle[indexNote]}</h2>
+    <br><p>- ${notes[indexNote]} 
+    <button onclick="deleteNote(${indexNote})"> x </button> </p><br>`;
 }
-function addNote () {
-    let noteInputRef = document.getElementById("note-input");
-    let noteInput = noteInputRef.value;
-    notes.push(noteInput);
-    renderNotes();
-    noteInputRef.value="";
+function addNote() {
+  let noteInputRef = document.getElementById("note-input");
+  let noteInput = noteInputRef.value;
+  let titleInputRef = document.getElementById("title-input");
+  let titleInput = titleInputRef.value;
+  notes.push(noteInput);
+  notesTitle.push(titleInput);
+  saveToLocalStorage();
+  renderNotes();
+  noteInputRef.value = "";
+  titleInputRef.value = "";
 }
 
-function deleteNote (indexNote) {
-    notes.splice(indexNote, 1);
-    renderNotes();
+function deleteNote(indexNote) {
+  notes.splice(indexNote, 1);
+  notesTitle.splice(indexNote, 1);
+  renderNotes();
+}
+function saveToLocalStorage() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("titles", JSON.stringify(notesTitle));
+}
+function getFromLocalStorage() {
+  const notesFromStorage = localStorage.getItem("notes");
+  let noteArr = JSON.parse(notesFromStorage);
+  if (noteArr != "") {
+    notes = noteArr;
+  }
+
+  const titlesFromStorage = localStorage.getItem("titles");
+  let titleArr = JSON.parse(titlesFromStorage);
+  if (titleArr != "") {
+    notesTitle = titleArr;
+  }
 }
